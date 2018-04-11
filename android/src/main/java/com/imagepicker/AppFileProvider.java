2f1307258703f30f61d404047cf03ec4015d7de8
Kyle <KyleZhong@etekcity.com.cn>
2018-04-11 15:01:37 +0800

    fixed android问题

---
+++ android/src/main/java/com/imagepicker/AppFileProvider.java
@@ -0,0 +1,12 @@
+package com.imagepicker;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Nick on 2017/8/28.
+ * Email：xingqiu93@163.com
+ * 自定义类 集成FileProvider 避免多个FileProvider冲突
+ */
+
+public class AppFileProvider extends FileProvider {
+}
